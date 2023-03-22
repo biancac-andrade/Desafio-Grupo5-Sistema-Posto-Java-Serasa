cdb1f320d841e312d8ff4c87fe1204444e68a3f6
Bianca Cristina <bc.andrade05@gmail.com>
2023-03-22 14:33:39 -0300

    Melhorias da classe abastecimento

--- src/Posto/servicos/Abastecimento.java
+++ src/Posto/servicos/Abastecimento.java
@@ -1,23 +1,58 @@
 package Posto.servicos;
 
+import java.util.Scanner;
+
+import Posto.pagamento.Pagamento;
+
+// Bianca Cristina - Cria classe de Abastecimento
 public class Abastecimento {
-    public static void Abastecer(int tipoCombustivel, double valorAbastecer){
+  public static void Abastecer() {
+      
+    Scanner entrada = new Scanner(System.in);
 
-        double precoGasolina = 4.75;
-        double precoEtanol = 3.85;
-        double precoDiesel = 5.15;
-        double qtdCombustivel;
+        double precoGasolina = 4.50;
+        double precoGasolinaAtivada = 6.60;
+        double precoEtanol = 3.50;
+        double precoDiesel = 5.60;
+        int tipoCombustivel;
+        double qtdCombustivel, valorAbastecer;
+      
+        System.out.println("Hoje temos seguinte tipo de combustiveis:");
+        System.out.println("[1] - Gasolina R$4,50 por litro ");
+        System.out.println("[2] - Etanol R$3,50 por litro ");
+        System.out.println("[3] - Diesel R$ 5,60 por litro ");
+        System.out.println("[4] - Gasolina ativada R$6,60 por litro ");
+        System.out.println("Digite o numero da sua opção:");
+        tipoCombustivel = entrada.nextInt();
 
+        System.out.println("Quanto voce gostaria de abastecer?");
+        valorAbastecer = entrada.nextDouble();
 
         if(tipoCombustivel == 1){
             qtdCombustivel = valorAbastecer / precoGasolina;
-            System.out.printf("Foi  abastecido %.2f, rendendo a quantidade de %.3f", qtdCombustivel, valorAbastecer);
+            System.out.printf("Ok, foi  abastecido %.2f litros de gasolina, rendendo o valor de R$ %.2f reais ",
+                qtdCombustivel, valorAbastecer);
+                Pagamento.formaPagamento(null);
         } else  if(tipoCombustivel == 2){
             qtdCombustivel = valorAbastecer / precoEtanol;
-            System.out.printf("Foi  abastecido %.2f, rendendo a quantidade de %.3f", qtdCombustivel, valorAbastecer);
-        }else{
+            System.out.printf("Ok, foi  abastecido %.2f litros de etanol, rendendo o valor de R$ %.2f reais ",
+                qtdCombustivel, valorAbastecer);
+                Pagamento.formaPagamento(null);
+          }else if(tipoCombustivel == 3){
             qtdCombustivel = valorAbastecer / precoDiesel;
-            System.out.printf("Foi  abastecido %.2f, rendendo a quantidade de %.3f", qtdCombustivel, valorAbastecer);
+            System.out.printf("Ok, foi  abastecido %.2f litros de diesel, rendendo o valor de R$ %.2f reais ",
+                qtdCombustivel, valorAbastecer);
+                Pagamento.formaPagamento(null);
+          } else if (tipoCombustivel == 4) {
+          qtdCombustivel = valorAbastecer / precoGasolinaAtivada;
+          System.out.printf("Ok, foi  abastecido %.2f litros de gasolina ativada, rendendo o valor de R$ %.2f reais ",
+              qtdCombustivel, valorAbastecer);
+                Pagamento.formaPagamento(null);
+          } else {
+          System.out.println("Essa opção não existe.");
         }
+
+
+        entrada.close();
     }
 }
