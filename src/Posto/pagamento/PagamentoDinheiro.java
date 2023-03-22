9013ce3759e8ce0de979ec6354e2bd2fcb01db78
Bianca Cristina <bc.andrade05@gmail.com>
2023-03-22 14:33:12 -0300

    Melhoria do codigo de pagamento em dinheiro

--- src/Posto/pagamento/PagamentoDinheiro.java
+++ src/Posto/pagamento/PagamentoDinheiro.java
@@ -1,14 +1,30 @@
 package Posto.pagamento;
 
+import java.util.Scanner;
+
+// Bianca Cristina reajusta classe Pagamento Dinheiro
 public class PagamentoDinheiro {
-    public static void pagamentoEmDinheiro(double quantiaEmDinheiro, double valorAbastecido){
+  private static double valorAbastecido;
+
+  public static void pagamentoEmDinheiro(String[] args) {
+
+      Scanner entrada = new Scanner(System.in);
+      
+      System.out.println("Sera pago no dinheiro");
+
+      System.out.println("Qual será a quantia paga?");
+      double quantiaEmDinheiro = entrada.nextDouble();
+
+
         if(quantiaEmDinheiro == valorAbastecido){
-            System.out.println("Obrigado, tenha um bom dia");
+            System.out.println("Obrigado, pagamento OK");
         }else if(quantiaEmDinheiro > valorAbastecido){
             double troco = quantiaEmDinheiro - valorAbastecido;
             System.out.println("Obrigado, seu troco é : " + troco);
         }else if(quantiaEmDinheiro < valorAbastecido){
-            System.out.println("Obrigado, nao vai rolar fiado não");
+            System.out.println("Obrigado, nao vai rolar fiado hoje não");
         }
     }
+
+  
 }
