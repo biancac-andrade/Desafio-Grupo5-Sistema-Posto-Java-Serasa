c35709798a85fe891e2acf649facd306d0dd908a
Bianca Cristina <bc.andrade05@gmail.com>
2023-03-27 18:24:59 -0300

    Reajuste classe de pagamento dinheiro

--- src/Posto/pagamento/PagamentoDinheiro.java
+++ src/Posto/pagamento/PagamentoDinheiro.java
@@ -2,29 +2,33 @@ package Posto.pagamento;
 
 import java.util.Scanner;
 
-// Bianca Cristina reajusta classe Pagamento Dinheiro
+// Bianca e Rick  reajusta classe Pagamento Dinheiro
 public class PagamentoDinheiro {
-  private static double valorAbastecido;
 
-  public static void pagamentoEmDinheiro(String[] args) {
+  public static void pagamentoEmDinheiro(double valorServico) {
 
       Scanner entrada = new Scanner(System.in);
       
       System.out.println("Sera pago no dinheiro");
 
       System.out.println("Qual será a quantia paga?");
-      double quantiaEmDinheiro = entrada.nextDouble();
+      double valorPago = entrada.nextDouble();
 
 
-        if(quantiaEmDinheiro == valorAbastecido){
+        if(valorPago == valorServico){
             System.out.println("Obrigado, pagamento OK");
-        }else if(quantiaEmDinheiro > valorAbastecido){
-            double troco = quantiaEmDinheiro - valorAbastecido;
+        }else if(valorPago > valorServico){
+            double troco = valorPago- valorServico;
             System.out.println("Obrigado, seu troco é : " + troco);
-        }else if(quantiaEmDinheiro < valorAbastecido){
+        }else if(valorPago < valorServico){
             System.out.println("Obrigado, nao vai rolar fiado hoje não");
         }
+		entrada.close();
+
     }
+
+  public static void dinheiro(double valorServico) {
+  }
 
   
 }
